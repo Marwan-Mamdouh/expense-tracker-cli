b42b2b64fe27e5794cdc9d62022b67f640fd7d8f
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-06-12 20:49:04 +0300

    add a new enum class to handle categoties

--- src/main/java/com/marwan/dev/expense_tracker/model/Category.java
+++ src/main/java/com/marwan/dev/expense_tracker/model/Category.java
@@ -1,4 +1,5 @@
 package com.marwan.dev.expense_tracker.model;
 
 public enum Category {
+  FOOD, FRUITS, INTERNET_BILL, TELEPHONE_BILL, ELECTRICITY_BILL, WATER_bill, GAS_BILL, CLEANING, GARBAGE, DEBTS, OTHER
 }
