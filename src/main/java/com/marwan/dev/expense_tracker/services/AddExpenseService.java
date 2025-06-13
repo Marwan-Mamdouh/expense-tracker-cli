a5f86e948a4cfd908aea7470916b5b3721104fee
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-06-13 23:47:59 +0300

    change Category helper method location and call the new one

--- src/main/java/com/marwan/dev/expense_tracker/services/AddExpenseService.java
+++ src/main/java/com/marwan/dev/expense_tracker/services/AddExpenseService.java
@@ -1,9 +1,8 @@
 package com.marwan.dev.expense_tracker.services;
 
-import com.marwan.dev.expense_tracker.exceptions.NotFoundCategoryException;
 import com.marwan.dev.expense_tracker.model.Category;
-import com.marwan.dev.expense_tracker.model.CreateExpenseDTO;
 import com.marwan.dev.expense_tracker.model.Expense;
+import com.marwan.dev.expense_tracker.model.dto.CreateExpenseDTO;
 import com.marwan.dev.expense_tracker.repository.ExpenseRepository;
 import org.springframework.stereotype.Service;
 
@@ -19,23 +18,6 @@ public class AddExpenseService implements CommandInterface<CreateExpenseDTO, Exp
   @Override
   public Expense execute(CreateExpenseDTO input) {
     return expenseRepository.save(
-        new Expense(input.description(), input.amount(), handleString(input.category())));
-  }
-
-  private Category handleString(String category) {
-    return switch (category.toUpperCase()) {
-      case "FOOD" -> Category.FOOD;
-      case "FRUITS" -> Category.FRUITS;
-      case "INTERNET_BILL" -> Category.INTERNET_BILL;
-      case "TELEPHONE_BILL" -> Category.TELEPHONE_BILL;
-      case "ELECTRICITY_BILL" -> Category.ELECTRICITY_BILL;
-      case "WATER_BILL" -> Category.WATER_bill;
-      case "GAS_BILL" -> Category.GAS_BILL;
-      case "CLEANING" -> Category.CLEANING;
-      case "GARBAGE" -> Category.GARBAGE;
-      case "DEBTS" -> Category.DEBTS;
-      case "OTHER" -> Category.OTHER;
-      default -> throw new NotFoundCategoryException(category);
-    };
+        new Expense(input.description(), input.amount(), Category.from(input.category())));
   }
 }
